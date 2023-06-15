c799430586ab4d179ccb19f2eab559c3a197280e
Urmas Öövel <uoovel@itcollege.ee>
2023-06-15 14:57:00 +0300

    Initial Setup

---
+++ src/main/java/com/codeurmas/orderapi/OrderapiApplication.java
@@ -0,0 +1,14 @@
+package com.codeurmas.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderapiApplication.class, args);
+		System.out.println("Orderapi started");
+	}
+
+}
